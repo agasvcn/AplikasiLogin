8e6f90d3799b3b990a3ec3cce9d5433e0b1241c2
agastya.selaksa <agastya.selaksa@dwidasa.com>
2025-05-02 17:42:31 +0700

    push aplikasi

---
+++ app/src/main/java/com/tugas/aplikasilogin/HomeActivity.kt
@@ -0,0 +1,19 @@
+package com.tugas.aplikasilogin
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val username = intent.getStringExtra("USERNAME")
+
+        val welcomeTextView = findViewById<TextView>(R.id.welcomeTextView)
+
+        welcomeTextView.text = "Selamat datang, $username!"
+    }
+}
+
---
+++ app/src/main/java/com/tugas/aplikasilogin/LoginActivity.kt
@@ -0,0 +1,30 @@
+package com.tugas.aplikasilogin
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val usernameEditText = findViewById<EditText>(R.id.usernameEditText)
+        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            val intent = Intent(this, HomeActivity::class.java)
+            intent.putExtra("USERNAME", username)
+
+            startActivity(intent)
+            finish()
+        }
+    }
+}
